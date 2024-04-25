090ae8af079357e6f89115c178369f2a9eea1ff8
Blackie01 <onioludamola@gmail.com>
2024-04-25 04:27:39 +0100

    checking

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" Component={Login} />
-        <Route path="/dashboard" Component={Dashboard} />
+        {/* <Route path="/dashboard" Component={Dashboard} /> */}
         <Route path="/dashboard/officiators" Component={ManageOfficiators} />
       </Routes>
     </Router>
